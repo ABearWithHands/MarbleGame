be03f989c60106dc9d2a5299892066d767a4af7c
ABearWithHands <timperry88@gmail.com>
2023-08-25 18:59:00 -0400

    1st Update

--- MarbleGame/Assets/Scripts/BallScript.cs
+++ MarbleGame/Assets/Scripts/BallScript.cs
@@ -1,24 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System;
 
 public class BallScript : MonoBehaviour
 {
     private Rigidbody rb;
-    [SerializeField] private GameObject target;
+    [SerializeField] private GameObject target = null;
     [SerializeField] float rollForce;
     [SerializeField] Transform centerPlatform;
+    [SerializeField] List<GameObject> enemyList = new List<GameObject>();
+    private GameManager gameManager;
     private Vector3 sweetSpot;
 
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
 
-    void Update()
+    void FixedUpdate()
     {
-        RollAtTarget();
+        if (target == null && gameManager.listOfBalls.Count > 1)
+        {
+            enemyList = ListEnemies(gameManager.listOfBalls);
+            target = NextTarget(enemyList);
+        }
+        else if (gameManager.listOfBalls.Count > 1)
+        {
+            RollAtTarget();
+        }
     }
 
     private void RollAtTarget()
@@ -27,9 +40,26 @@ public class BallScript : MonoBehaviour
         //Vector3 distanceVector = targetLocation - this.gameObject.transform.position;
         //rb.AddForceAtPosition(rollForce * distanceVector.normalized, transform.position, ForceMode.Impulse);
 
-        Vector3 targetLocation = target.GetComponent<BallScript>().DetermineSweetSpot();
+        //Vector3 targetLocation = target.GetComponent<BallScript>().DetermineSweetSpot();
+        Vector3 targetLocation = target.transform.position;
         Vector3 distanceVector = targetLocation - this.gameObject.transform.position;
         rb.AddForceAtPosition(rollForce * distanceVector.normalized, transform.position, ForceMode.Impulse);
+    }
+
+    private List<GameObject> ListEnemies(List<GameObject> list)
+    {
+        var enemiesList = from enemy in list
+                          where enemy != this.gameObject
+                          select enemy;
+
+        return enemiesList.ToList();
+    }
+
+    private GameObject NextTarget(List<GameObject> listOfEnemies)
+    {
+        System.Random rnd = new System.Random();
+
+        return listOfEnemies[rnd.Next(enemyList.Count)];
     }
 
     public Vector3 DetermineSweetSpot()
--- MarbleGame/Assets/Scripts/GameManager.cs
+++ MarbleGame/Assets/Scripts/GameManager.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<GameObject> listOfBalls = new List<GameObject>();
+    [SerializeField] float distanceFromCenter;
+    //public GameObject[] listOfBalls;
+   
     void Start()
     {
-        
+        listOfBalls.AddRange(GameObject.FindGameObjectsWithTag("Ball"));
+        SetBalls();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetBalls()
     {
-        
+        for (int i = 0; i < listOfBalls.Count; i++)
+        {
+            float j = i;
+            listOfBalls[i].transform.position = new Vector3((float)(distanceFromCenter * Math.Cos((double)(j / listOfBalls.Count) * Math.PI * 2)), 3.5f, (float)(distanceFromCenter * Math.Sin((double)(j / listOfBalls.Count) * Math.PI * 2)));
+            
+        }
     }
 }
